f63d089e288b1cf8963f227fef34306a1b119027
Hans Arias <hearias6@misena.edu.co>
2016-11-27 22:34:51 -0500

    agregar mocha [framework unit test], librerias como chai y chai-http, realizar un test de prueba

---
+++ controllers/practicaController.js
@@ -0,0 +1,18 @@
+
+var app = {
+
+  sumar: function(num1, num2) {
+    return num1 + num2;
+  },
+
+  restar: function(num1, num2) {
+    return num1 - num2;
+  },
+
+  multiplicar: function(num1, num2) {
+    return num1 * num2;
+  }
+
+}
+
+module.exports = app;
